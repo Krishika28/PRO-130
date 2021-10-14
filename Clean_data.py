004b5d4c2eaecd1d97dc097a473ae55878cee226
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2021-10-14 12:29:18 +0530

    Add files via upload

---
+++ Clean_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("merged_data.csv")
+
+del df["Distance"]
+del df["Mass"]
+del df["Radius"]
+del df["Star_name"]
+
+print(df.columns)
+
+df.to_csv("main.csv")
+
+data = pd.read_csv("main.csv")
+
+print(df[0:5])
