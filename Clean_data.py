e57e4d40c1a3710272991e9a77c51f5168836a49
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2021-10-17 12:59:13 +0530

    Add files via upload

--- Clean_data.py
+++ Clean_data.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv("merged_data.csv")
-
-
-del df["Distance"]
-del df["Mass"]
-del df["Radius"]
-del df["Star_name"]
-
+print(df[0:5])
 print(df.columns)
+
+df.drop(["Unnamed: 0", "Star_name.1", "Luminosity","Distance.1", "Radius.1", "Mass.1" ], axis=1, inplace=True)
+df = df.dropna()
+
 
 df.to_csv("main.csv")
 
